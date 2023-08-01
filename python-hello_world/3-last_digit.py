d2c7f9ea5aac597c4ade6f440ab533f506c119ff
elizabethwanjiku703 <elizabethwanjiku703@gmail.com>
2023-08-01 01:38:03 -0700

    added the file

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 # Print the required output
-print("Last digit of", number, "is", last_digit, end=" ")
+print("Last digit of {} is {} ".format(number, last_digit), end="")
 if last_digit > 5:
     print("and is greater than 5")
 elif last_digit == 0:
