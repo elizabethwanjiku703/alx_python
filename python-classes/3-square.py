ec8e4204a4cb4f09181b204c74aa2356bae99124
Elizabeth Waweru <66907478+elizabethwanjiku703@users.noreply.github.com>
2023-08-23 20:05:20 +0300

    Update 3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-class Square:
-    """
-    This class represents a square.
 
+""" 
+This class represents a square.
+"""
+class Square:
+    
+    """
     Attributes:
         __size (int): The size of the square.
 
@@ -48,13 +51,16 @@ class Square:
 
         Args:
             size (int): The size of the square.
+        """
+        
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
 
+        """
         Raises:
             TypeError: If the provided size is not an integer.
             ValueError: If the provided size is less than 0.
         """
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
