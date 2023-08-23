bd718c88b2134950f12fbb6b7c4d1b4bd9e477a9
Elizabeth Waweru <66907478+elizabethwanjiku703@users.noreply.github.com>
2023-08-23 20:02:28 +0300

    Update 3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,30 +1,40 @@
 #!/usr/bin/python3
-
-# Define the Square class
 class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size=0): Initializes a new instance of the Square class.
+        area(self): Calculates and returns the area of the square.
+        size(self): Getter method to retrieve the size of the square.
+        size(self, value): Setter method to set the size of the square.
+    """
+
     def __init__(self, size=0):
         """
-        Constructor method for the Square class.
-        Initializes the size attribute of the object.
+        Initializes a new instance of the Square class.
 
         Args:
-            size (int): The size of the square (default is 0).
+            size (int): The size of the square. Defaults to 0.
         """
         self.__size = size
 
     def area(self):
         """
-        Calculate the area of the square.
+        Calculates and returns the area of the square.
 
         Returns:
             int: The area of the square.
         """
-        return self.__size**2
+        return self.__size ** 2
 
     @property
     def size(self):
         """
-        Getter method for the size attribute.
+        Getter method to retrieve the size of the square.
 
         Returns:
             int: The size of the square.
@@ -34,18 +44,18 @@ class Square:
     @size.setter
     def size(self, size):
         """
-        Setter method for the size attribute.
-        Checks if the new size is a positive integer.
+        Setter method to set the size of the square.
 
         Args:
-            size (int): The new size of the square.
+            size (int): The size of the square.
 
         Raises:
-            TypeError: If the new size is not an integer.
-            ValueError: If the new size is less than 0.
+            TypeError: If the provided size is not an integer.
+            ValueError: If the provided size is less than 0.
         """
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
+
