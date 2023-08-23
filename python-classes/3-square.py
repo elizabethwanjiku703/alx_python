87cf3a87ac511d5ca3057376bd6fd3168f2960d2
elizabethwanjiku703 <elizabethwanjiku703@gmail.com>
2023-08-23 09:56:49 -0700

    Access and update private attribute

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,41 +1,51 @@
 #!/usr/bin/python3
+
+# Define the Square class
 class Square:
-    """
-    This line defines a class named "Square".
-    """
     def __init__(self, size=0):
-        self.__size = size
-        """This line checks if the "size" parameter is an instance of the "int" class. 
-        This line assigns the value of the "size" parameter to the instance variable "__size".
         """
+        Constructor method for the Square class.
+        Initializes the size attribute of the object.
+
+        Args:
+            size (int): The size of the square (default is 0).
+        """
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size**2
+
     @property
     def size(self):
-            """The @property decorator is used to define a getter method for the size attribute. 
-            This allows to access the size attribute as if it were a regular attribute, 
-            using the dot notation (square.size).
-            """
-            return self.__size
-    """
-    The @size.setter decorator is used to define a setter method for the size attribute.
-    """
-    @size.setter
-    def size (self, value):
-        """ 
-        This allows to update the size attribute using the assignment operator 
-        (square.size = new_size).
         """
-        if not isinstance (value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
-        """Inside the setter method, type and value checks are performed to ensure that the 
-        size attribute is an integer and greater than or equal to 0. If these checks fail, 
-        appropriate exceptions (TypeError and ValueError) are raised.
-        """
-    def area(self):
-             """
-             The area method calculates and returns the area of the square.
-             """
-             return self.__size **2
+        Getter method for the size attribute.
 
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """
+        Setter method for the size attribute.
+        Checks if the new size is a positive integer.
+
+        Args:
+            size (int): The new size of the square.
+
+        Raises:
+            TypeError: If the new size is not an integer.
+            ValueError: If the new size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
