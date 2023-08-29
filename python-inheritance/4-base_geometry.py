e24dabc955b42b9dfe3acca2537a021fa267acf8
Elizabeth Waweru <elizabethwanjiku703@gmail.com>
2023-08-29 04:45:33 -0700

    Improve Geometry

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module for the BaseGeometry class.
+"""
+class BaseGeometry:
+    def area(self):
+        """
+        Placeholder method for calculating the area.
+        
+        Raises:
+            Exception: This method is not implemented in the base class.
+        """
+        raise Exception('area() is not implemented')
