632a71ee22b006f028c74b197c7f5385e8d31895
Elizabeth Waweru <elizabethwanjiku703@gmail.com>
2023-08-29 04:11:24 -0700

    Only sub class of

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,15 +1,17 @@
-def is_kind_of_class(obj, a_class):
+#!/usr/bin/python3
+"""
+Module for checking if an object inherits from a specified class.
+"""
+
+def inherits_from(obj, a_class):
     """
-    Check if an object is an instance of a specified class or any of its subclasses.
+    Check if obj inherits from a_class.
 
     Args:
         obj: The object to check.
         a_class: The specified class to compare against.
 
     Returns:
-        True if the object is an instance of the specified class or any of its subclasses; False otherwise.
+        True if obj inherits from a_class, False otherwise.
     """
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    return issubclass(type(obj), a_class) and type(obj) != a_class
