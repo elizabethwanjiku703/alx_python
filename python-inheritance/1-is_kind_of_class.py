7fa75069c510adf9065195cb2903fd7c4c3aea86
Elizabeth Waweru <elizabethwanjiku703@gmail.com>
2023-08-29 02:11:41 -0700

    Same class or inherit from

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""
+This module provides a function to check if an object is an instance of a specified class or any of its subclasses.
+"""
+
 def is_kind_of_class(obj, a_class):
     """
     Check if an object is an instance of a specified class or any of its subclasses.
@@ -13,3 +18,4 @@ def is_kind_of_class(obj, a_class):
         return True
     else:
         return False
+
