6d2267ea4addc7b1e4fc263c6698d227b08e8516
Elizabeth Waweru <elizabethwanjiku703@gmail.com>
2023-08-29 05:21:14 -0700

    Geometry module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""
-Module for the BaseGeometry class.
-"""
-
+"""Module for the BaseGeometry class."""
 class BaseGeometry:
     """
     This is the BaseGeometry class.
-    
     It serves as the base for geometry-related classes.
     """
     pass
+
