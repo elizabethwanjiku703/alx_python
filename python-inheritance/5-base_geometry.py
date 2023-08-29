b7536f2e695670a0e7808809cd77c2c982c224bf
Elizabeth Waweru <elizabethwanjiku703@gmail.com>
2023-08-29 05:35:05 -0700

    class BaseGeometry:

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
-"""A class representing a base geometry."""
 class BaseGeometry:
+    """
+    A class representing a base geometry.
+
+    Methods:
+    - area(): Public instance method that raises an Exception with the message area() is not implemented.
+    - integer_validator(name, value): Public instance method that validates an integer value.
+
+    Attributes: None
+    """
+
     def area(self):
-        """Public instance method that raises an Exception."""
+        """
+        Public instance method that raises an Exception with the message area() is not implemented.
+        """
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
-        """Public instance method that validates the 'value'.
+        """
+        Public instance method that validates the 'value'.
+
+        Parameters:
+        - name: A string representing the name of the value.
+        - value: The value to be validated.
+
         Raises:
         - TypeError: If the 'value' is not an integer.
         - ValueError: If the 'value' is less than or equal to 0.
