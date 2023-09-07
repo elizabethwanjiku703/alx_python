75562217640112ac92822d9af0d41faa9e3ed1da
Elizabeth Waweru <66907478+elizabethwanjiku703@users.noreply.github.com>
2023-09-07 16:23:39 +0300

    Update base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""Base class"""
+""" Base module contains class Base """
+
 class Base:
+    """Base class for all classes"""
+    
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initializes instance class"""
+        """Initializes instance class base id"""
         if id is not None:
             self.id = id
         else:
